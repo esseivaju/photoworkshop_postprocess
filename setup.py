448789c6da6147bd244b10dcd07ee7ebda9ef02c
Julien Esseiva <esseivaju@gmail.com>
2020-07-10 10:20:52 +0200

    description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="photoworkshop_postprocess",
     author="Julien Esseiva",
-    description="Find a filename in image files in the input directory.",
+    description="Find a filename in image files in the input directory and rename each image file with the filename found",
     version="0.1",
     packages=find_packages(),
     scripts=["bin/photoworkshop_postprocess"],
